609a2b6c29fbcafa3fac04131c2f717fb300bd01
bengel07 <bengel.bg@gmail.com>
2026-08-03 20:57:46 -0400

    Update application with latest changes

--- database.py
+++ database.py
@@ -8,5 +8,16 @@ def init_db(app):
     db.init_app(app)
 
 
+def migration_prochaine_echeance():
+    try:
+        db.session.execute(text("""
+            ALTER TABLE prets
+            ADD COLUMN IF NOT EXISTS prochaine_echeance DATE;
+        """))
 
+        db.session.commit()
+        print("✅ Colonne prochaine_echeance ajoutée")
 
+    except Exception as e:
+        db.session.rollback()
+        print("❌ Erreur migration prochaine_echeance :", e)
