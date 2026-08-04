f2e555b8fc2a2018e3a591d6e12ad632c4ea4936
bengel07 <bengel.bg@gmail.com>
2026-08-04 15:13:37 -0400

    Update application with latest changes

--- database.py
+++ database.py
@@ -1,3 +1,5 @@
+import os
+
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
 
@@ -5,8 +7,14 @@ db = SQLAlchemy()
 
 
 def init_db(app):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'sqlite:///gmes.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     db.init_app(app)
 
-
+    # Créer toutes les tables si elles n'existent pas
+    with app.app_context():
+        db.create_all()
+        print("✅ Tables créées avec succès")
 
 
