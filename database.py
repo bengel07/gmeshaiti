625e931fb5206fdc337ee73ce6044065e6b525d7
bengel07 <bengel.bg@gmail.com>
2026-08-03 21:11:27 -0400

    Update application with latest changes

--- database.py
+++ database.py
@@ -7,17 +7,14 @@ db = SQLAlchemy()
 def init_db(app):
     db.init_app(app)
 
-
-def migration_prochaine_echeance():
-    try:
-        db.session.execute(text("""
-            ALTER TABLE prets
-            ADD COLUMN IF NOT EXISTS prochaine_echeance DATE;
-        """))
-
-        db.session.commit()
-        print("✅ Colonne prochaine_echeance ajoutée")
-
-    except Exception as e:
-        db.session.rollback()
-        print("❌ Erreur migration prochaine_echeance :", e)
+    with app.app_context():
+        try:
+            db.session.execute(text("""
+                ALTER TABLE prets
+                ADD COLUMN IF NOT EXISTS prochaine_echeance DATE;
+            """))
+            db.session.commit()
+            print("✅ Colonne prochaine_echeance vérifiée")
+        except Exception as e:
+            db.session.rollback()
+            print("❌ Erreur migration prochaine_echeance :", e)
