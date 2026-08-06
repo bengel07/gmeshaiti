48b82047b664f092a8ea89606f0a77b700382034
bengel07 <bengel.bg@gmail.com>
2026-08-05 21:02:17 -0400

    Update application with latest changes

--- database.py
+++ database.py
@@ -1,16 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
 
-
 db = SQLAlchemy()
+
 
 def init_db(app):
     db.init_app(app)
 
-    with app.app_context():
 
 
-        db.drop_all()
-        db.create_all()
 
-        print("✅ Toutes les tables ont été recréées.")
