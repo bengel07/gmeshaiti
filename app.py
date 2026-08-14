30fcce31fed212bb17e5fbd059e9ba6eb145a5c3
bengel07 <bengel.bg@gmail.com>
2026-08-14 09:11:52 -0400

    Update application with latest changes

--- app.py
+++ app.py
@@ -14567,7 +14567,8 @@ def remboursements_retards(succursale_code):
         )
 
 
-
+@app.route('/<succursale_code>/remboursements/export-retards')
+@login_required
 def export_remboursements_retards():
     """
     Exporte la liste des remboursements en retard vers un fichier Excel.
