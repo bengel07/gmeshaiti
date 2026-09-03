903bd83e8da8bbc229c8b3c0c23b7261128f0c1c
bengel07 <bengel.bg@gmail.com>
2026-09-03 19:18:50 -0400

    Configure new PostgreSQL database

--- app.py
+++ app.py
@@ -180,7 +180,7 @@ if os.environ.get('DATABASE_URL'):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 else:
     # Développement local
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///gmes.db1'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///gmes.db2'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SESSION_PERMANENT'] = False
 app.config['SESSION_TYPE'] = 'filesystem'
@@ -314,7 +314,7 @@ async def track(data: dict):
     lat = data["lat"]
     lon = data["lon"]
 
-    conn = sqlite3.connect("gmes.db1")
+    conn = sqlite3.connect("gmes.db2")
     cur = conn.cursor()
 
     cur.execute("""
