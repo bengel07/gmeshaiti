e08b84fa65a16b922f76c7ade317fcf11cc1ebfd
bengel07 <bengel.bg@gmail.com>
2026-09-03 21:15:38 -0400

    Update application with latest changes

--- database.py
+++ database.py
@@ -1,17 +1,7 @@
-init_db(app)
+from flask_sqlalchemy import SQLAlchemy
 
-with app.app_context():
-    creer_produit_epargne_defaut()
+db = SQLAlchemy()
 
-    db.create_all()
 
-    print("✅ Tables vérifiées/créées")
-
-    # Vérifier les clients disponibles
-    client_admin = Client.query.first()
-
-    print(
-        f"👤 Client disponible : {client_admin.id}"
-        if client_admin
-        else "⚠️ Aucun client dans la base"
-    )
+def init_db(app):
+    db.init_app(app)
